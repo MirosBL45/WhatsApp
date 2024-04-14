99167b7d97c49864780317c6c9aab5f4146d8bf6
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-04-14 17:12:28 +0200

    connected convex and clerk

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a WhatsUp Clone made with Next.js, TypeScript, TailwindCSS, shadcn-ui, Convex for DataBase, Clerk for Authentication
+This is a WhatsUp Clone made with Next.js, TypeScript, TailwindCSS, shadcn-ui, Convex for DataBase, Clerk for Authentication, Svix for sending webhooks easy and reliable
---
+++ convex/users.ts
@@ -0,0 +1,20 @@
+import { query, internalMutation } from './_generated/server';
+import { v } from 'convex/values';
+
+export const createUser = internalMutation({
+  args: {
+    tokenIdentifier: v.string(),
+    email: v.string(),
+    name: v.string(),
+    image: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert('users', {
+      tokenIdentifier: args.tokenIdentifier,
+      email: args.email,
+      name: args.name,
+      image: args.image,
+      isOnline: true,
+    });
+  },
+});
