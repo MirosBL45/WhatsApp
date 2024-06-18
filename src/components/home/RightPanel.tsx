f3f2cf40a698561d3dbc4baa877eb5eb6c77b6f4
MirosBL45 <jovicmiroslav88@gmail.com>
2024-06-18 15:12:43 +0200

    right panel kick

--- src/components/home/RightPanel.tsx
+++ src/components/home/RightPanel.tsx
@@ -7,13 +7,21 @@ import MessageInput from './MessageInput';
 import ChatPlaceholder from './ChatPlaceholder';
 import GroupMembersDialog from './GroupMembersDialog';
 import { useConversationStore } from '@/store/chat-store';
+import { useConvexAuth } from 'convex/react';
 
 export default function RightPanel() {
-  const { selectedConversation, setSelectedConversation } = useConversationStore();
+  const { selectedConversation, setSelectedConversation } =
+    useConversationStore();
+
+  const { isLoading } = useConvexAuth();
+
+  if (isLoading) return null;
   if (!selectedConversation) return <ChatPlaceholder />;
 
-  const conversationName = selectedConversation.groupName || selectedConversation.name;
-  const conversationImage = selectedConversation.groupImage || selectedConversation.image;
+  const conversationName =
+    selectedConversation.groupName || selectedConversation.name;
+  const conversationImage =
+    selectedConversation.groupImage || selectedConversation.image;
 
   return (
     <div className="w-3/4 flex flex-col">
@@ -22,14 +30,21 @@ export default function RightPanel() {
         <div className="flex justify-between bg-gray-primary p-3">
           <div className="flex gap-3 items-center">
             <Avatar>
-              <AvatarImage src={conversationImage || '/placeholder.png'} className="object-cover" />
+              <AvatarImage
+                src={conversationImage || '/placeholder.png'}
+                className="object-cover"
+              />
               <AvatarFallback>
                 <div className="animate-pulse bg-gray-tertiary w-full h-full rounded-full" />
               </AvatarFallback>
             </Avatar>
             <div className="flex flex-col">
               <p>{conversationName}</p>
-              {selectedConversation.isGroup && <GroupMembersDialog selectedConversation={selectedConversation} />}
+              {selectedConversation.isGroup && (
+                <GroupMembersDialog
+                  selectedConversation={selectedConversation}
+                />
+              )}
             </div>
           </div>
 
@@ -37,7 +52,11 @@ export default function RightPanel() {
             <a href="/video-call" target="_blank">
               <Video size={23} />
             </a>
-            <X size={16} className="cursor-pointer" onClick={() => setSelectedConversation(null)} />
+            <X
+              size={16}
+              className="cursor-pointer"
+              onClick={() => setSelectedConversation(null)}
+            />
           </div>
         </div>
       </div>
